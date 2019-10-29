4ddb32b64cae0feeacf1ddb0a18dc90d608bd456
postman17 <frompostal@gmail.com>
2019-10-29 17:01:55 +0500

    schema of project

---
+++ frontend/src/components/Article.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Подробнее</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Article"
+    }
+</script>
+
+<style scoped>
+.card {
+    margin-top: 20px;
+    width: 100%;
+}
+</style>
---
+++ frontend/src/components/ArticleList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="list-group">
+        <Article />
+        <Article />
+        <Article />
+        <Article />
+        <Article />
+    </div>
+</template>
+
+<script>
+    import Article from '../components/Article'
+
+    export default {
+        name: "ArticleList",
+        components: {
+            Article
+        }
+    }
+</script>
+
+<style scoped>
+</style>
